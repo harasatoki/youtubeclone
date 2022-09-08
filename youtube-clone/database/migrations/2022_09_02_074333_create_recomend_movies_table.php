40ec01b371fc68ceae06bd55b055a3389b8513af
harasatoki <101545841+harasatoki@users.noreply.github.com>
2022-09-08 19:42:58 +0900

    マイグレーションファイルです。

---
+++ youtube-clone/database/migrations/2022_09_02_074333_create_recomend_movies_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recomend_movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->integer('entertainment')->default(1);
+            $table->integer('education')->default(1);
+            $table->integer('music')->default(1);
+            $table->integer('animal')->default(1);
+            $table->integer('streamer')->default(1);
+            $table->integer('society')->default(1);
+
+            $table->index('user_id');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recomend_movies');
+    }
+};
