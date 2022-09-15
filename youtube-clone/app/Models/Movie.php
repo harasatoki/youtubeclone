02c44bbb145a05871a544ed65e435f60da45d834
harasatoki <101545841+harasatoki@users.noreply.github.com>
2022-09-15 18:53:04 +0900

    カテゴリーIDから動画を取得

---
+++ youtube-clone/app/Models/Movie.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'movie',
+        'thumbnails_image',
+        'movie_title',
+        'movie_outline',
+        'type1',
+        'type2',
+        'type3',
+    ];
+
+    /**
+     * ユーザーリレーション
+     *
+     * @return void
+     */
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * ムービーカテゴリーリレーション
+     *
+     * @return void
+     */
+    public function movieCategory(){
+        return $this->hasMany(MovieCategory::class);
+    }
+
+    /**
+     * 視聴履歴のリレーション
+     *
+     * @return void
+     */
+    public function viewsTimes(){
+        return $this->hasOne(ViewsTime::class);
+    }
+
+    /**
+     * カテゴリーリストから動画を取得
+     *
+     * @param array $categoryIds
+     * 
+     * @return array
+     */
+    public function fetchMoviesFromCategoryIds($categoryIds){
+        $recomendMovies = collect([]);
+        $testlist = array();
+        foreach($categoryIds as $categoryId){
+            $i=0;
+            while($i<=5){
+                $recomendMovie = $this
+                    ->inRandomOrder()
+                    ->with('user')
+                    ->with('movieCategory')
+                    ->with('viewstimes')
+                    ->whereHas('movieCategory',function($fhechMovieCategoryId)use($categoryId){
+                        $fhechMovieCategoryId->where('movie_category_id', $categoryId);
+                    })
+                    ->first();
+                    if(!in_array($recomendMovie->id, $testlist)){
+                        $recomendMovies = $recomendMovies->concat([$recomendMovie]);
+                        $testlist[] = $recomendMovie->id;
+                    }
+                    $i++;
+                }
+        }
+    
+        return $recomendMovies;
+    }
+
+    /**
+     * ユーザーの投稿動画を取得
+     *
+     * @param int $userId
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function fetchPostedVideo($userId)
+    {
+        return $this->with('user')->with('viewstimes')->where('user_id', $userId)->get();
+    }
+}
